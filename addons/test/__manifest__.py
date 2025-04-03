c7b35880d29dc572fac320f25b72cb55c54368e4
Ajlal Hasan <101212675+ajlalhasan@users.noreply.github.com>
2025-04-04 01:16:11 +0500

    Update __manifest__.py

--- addons/test/__manifest__.py
+++ addons/test/__manifest__.py
@@ -1,1 +1,13 @@
-
+{
+    'name': 'Test Module',
+    'version': '1.0',
+    'author': 'Your Name',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/test_model_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
