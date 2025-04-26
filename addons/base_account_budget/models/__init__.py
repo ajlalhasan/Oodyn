5ef45c37ba71dc3ffe13602c6a2206c560695264
Ajlal Hasan <101212675+ajlalhasan@users.noreply.github.com>
2025-04-26 23:30:07 +0500

    Add files via upload

--- addons/base_account_budget/models/__init__.py
+++ addons/base_account_budget/models/__init__.py
@@ -19,19 +19,5 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
-from odoo import api, fields, models
-
-
-class AccountAnalyticAccount(models.Model):
-    """Inherits the AccountAnalytic model to add new budget line field that
-    connect with the budget line modules"""
-    _inherit = "account.analytic.account"
-
-    budget_line = fields.One2many('budget.lines',
-                                  'analytic_account_id',
-                                  'Budget Lines')
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        res = super().create(vals_list)
-        return res
+from . import account_budget
+from . import account_analytic_account
