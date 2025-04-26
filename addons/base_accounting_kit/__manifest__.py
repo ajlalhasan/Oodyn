6fe4f6f4972bb8e661869c8fb8492b79d05e870b
Ajlal Hasan <101212675+ajlalhasan@users.noreply.github.com>
2025-04-27 00:50:06 +0500

    Update __manifest__.py

--- addons/base_accounting_kit/__manifest__.py
+++ addons/base_accounting_kit/__manifest__.py
@@ -97,7 +97,7 @@
         'wizard/import_bank_statement_views.xml',
     ],
     'external_dependencies': {
-            'python': ['openpyxl', 'ofxparse']
+            'python': ['openpyxl', 'ofxparse', 'qifparse']
         },
     'assets': {
         'web.assets_backend': [
